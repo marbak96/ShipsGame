5016a884b11a1fb5028f038356a38ef63ce0e8ed
Marcin <marbak151196@gmail.com>
2019-02-26 22:29:04 +0100

    system

--- src/shipsgame/Account.java
+++ src/shipsgame/Account.java
@@ -1,4 +1,4 @@
-/*
+﻿/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -39,6 +39,7 @@ public class Account {
         return mark;  
         }
     
+System.out.println("Nie ma takiego uzytkownika ! ");
     public static void CreateAnAccount()
 	{
            String  Login, Password;
